aee036a80dcde63a5fea557fc397c74ce8b7b28f
gökhan <gokhanyanik@gmail.com>
2024-11-13 15:38:50 +0300

    SignIn.tsx Screen Design Added

--- screens/SignIn.tsx
+++ screens/SignIn.tsx
@@ -1,19 +1,75 @@
-import React from "react";
-import { View, Text, Button, StyleSheet } from 'react-native';
+import React, { useState } from "react";
+import { View, Text, Button, StyleSheet, TextInput, TouchableOpacity,Image } from 'react-native';
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import { useNavigation } from "@react-navigation/native";
 import { RootStackParamList } from "../types";
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+
 //signIn için tip tanımlaması...
 type SignInScreenNavigationProps = NativeStackNavigationProp<RootStackParamList, 'SignIn'>;
 
 const SignIn = () => {
     const navigation = useNavigation<SignInScreenNavigationProps>();
-    return (
-        <View style={styles.container}>
-            <Text style={styles.title}>Sign In Screen</Text>
-            <Button title="Go to Sign Up" onPress={() => navigation.navigate('SignUp')} />
-            <Button title="Go to HomeScreen" onPress={() => navigation.navigate('HomeScreen')} />
 
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+    const togglePasswordVisibility = () => {
+        setIsPasswordVisible(!isPasswordVisible);
+    };
+    return (
+        <View style={{ flex: 1 }}>
+            <View style={{justifyContent:"flex-start",alignItems:"center",margin:50}}>
+            <Image
+            source={require('../assets/images/todolistSignIn.png')}
+            style={styles.image}
+            />
+            </View>
+            
+            <View style={styles.container}>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Email"
+                    keyboardType="email-address"
+                    autoCapitalize="none"
+                    value={email}
+                    onChangeText={setEmail}
+                />
+                <View style={styles.inputView}>
+                    <TextInput
+                        style={styles.inputPassword}
+                        placeholder="Password"
+                        secureTextEntry={!isPasswordVisible}
+                        value={password}
+                        onChangeText={setPassword}
+                    />
+                    <TouchableOpacity style={styles.icon} onPress={togglePasswordVisibility}>
+                        <Icon
+                            name={isPasswordVisible ? "eye-off" : "eye"} // Şifre görünürlüğüne göre simge değişir
+                            size={24}
+                            color="gray"
+                        />
+                    </TouchableOpacity>
+
+                </View>
+                <View style={{ width: '100%', alignItems: "flex-end", right: 10,margin:15}}>
+                    <TouchableOpacity  onPress={() => navigation.navigate("ForgotPassword")}>
+                        <Text>Forgot Password?</Text>
+                    </TouchableOpacity>
+                </View>
+                <TouchableOpacity style={styles.touchable} onPress={() => navigation.navigate('SignUp')}>
+                    <Text style={{ color: "white" }}>SIGN IN</Text>
+                </TouchableOpacity>
+
+                <View style={{ flexDirection: "row", justifyContent: "center" }}>
+                    <Text>Don't have an account? </Text>
+                    <TouchableOpacity style={{ width: 90, height: 50 }} onPress={() => navigation.navigate('SignUp')}>
+                        <Text style={{ color: "#FF8A80" }}>SIGN UP</Text>
+                    </TouchableOpacity>
+                </View>
+
+            </View>
         </View>
     )
 }
@@ -25,10 +81,56 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         backgroundColor: '#f0f0f0',
     },
+    image:{
+        width: 200,
+        height: 200,
+        resizeMode:"contain"
+    },
     title: {
         fontSize: 24,
         fontWeight: 'bold',
         marginBottom: 20,
     },
+    inputView: {
+        width: 370,
+        height: 50,
+        borderColor: '#ccc',
+        flexDirection: "row",
+        borderWidth: 1,
+        borderRadius: 15
+    },
+    input: {
+        width: 370,
+        height: 50,
+        borderColor: '#ccc',
+        borderWidth: 1,
+        marginBottom: 15,
+        paddingHorizontal: 10,
+        borderRadius: 15,
+    },
+    inputPassword: {
+        width: 350,
+        height: 50,
+        // borderColor: '#ccc',
+        // borderWidth: 1,
+        marginBottom: 15,
+        paddingHorizontal: 10,
+        borderRadius: 15,
+    },
+    touchable: {
+        width: 370,
+        height: 50,
+        borderColor: '#ccc',
+        borderWidth: 1,
+        marginBottom: 15,
+        justifyContent: "center",
+        alignItems: "center",
+        borderRadius: 15,
+        backgroundColor: "#FF8A80",
+        margin: 15,
+    },
+    icon: {
+        margin: 5
+    }
 })
 export default SignIn;
