19aae60e534b05b11de4935a5d4ecad742d26b95
gökhan <gokhanyanik@gmail.com>
2024-12-09 16:58:48 +0300

    HomeScreen.tsx Screen Design Added

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, Text, StyleSheet, Button ,TouchableOpacity,Image} from 'react-native';
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import { useNavigation } from "@react-navigation/native";
 import { RootStackParamList } from "../types";
@@ -11,11 +11,50 @@ const HomeScreen = () => {
    
     return (
         <View style={styles.container}>
-            <Text style={styles.title}>Home Screen</Text>
-            <Button title="Go to AddTodo" onPress={() => navigation.navigate('AddTodo')} />
-            <Button title="Go to DetailTodo" onPress={() => navigation.navigate('DetailTodo')} />
-            <Button title="Go to EditTodo" onPress={() => navigation.navigate('EditTodo')} />
-            <Button title="Go to ProfilScreen" onPress={() => navigation.navigate('ProfilScreen')} />
+            <View style={{ flexDirection: "row", width: "100%", height: 40, backgroundColor: "white", justifyContent: "space-between", margin: 15 }}>
+                <TouchableOpacity style={{ backgroundColor: "white" }} onPress={() => console.log("todolistProfilScreen")}>
+                    <Image
+                        source={require('../assets/images/todolistProfilScreen.png')}
+                        style={{ width: 83, height: 18, margin: 10 }}
+                    />
+                </TouchableOpacity>
+                <TouchableOpacity style={{ backgroundColor: "white" }} onPress={() => console.log("ayarlarIkonProfilScreen")}>
+                    <Image
+                        source={require('../assets/images/ayarlarIkonProfilScreen.png')}
+                        style={{ width: 22, height: 22, margin: 15 }}
+                    />
+                </TouchableOpacity>
+            </View>
+            <View style={{ flexDirection: "row", width: "100%", height: 40, backgroundColor: "white", justifyContent: "space-between", margin: 10 }}>
+                <TouchableOpacity style={{ backgroundColor: "white" ,justifyContent:"flex-start"}} onPress={() => console.log("todolistProfilScreen")}>
+                    <Image
+                        source={require('../assets/images/todo.png')}
+                        style={{ width: 50, height:35 ,resizeMode:"contain",margin:7}}
+                    />
+                </TouchableOpacity>
+                <TouchableOpacity style={{ backgroundColor: "white" }} onPress={() => console.log("ayarlarIkonProfilScreen")}>
+                    <Image
+                        source={require('../assets/images/filtre.png')}
+                        style={{ width: 22, height: 22, margin: 15 }}
+                    />
+                </TouchableOpacity>
+            </View>
+            <View style={{backgroundColor:"#F76C6A",height:150,width:"90%",margin:10,borderRadius:20,top:10}}>
+                    <Text style={{margin:10,color:"white",fontSize:17,fontWeight:"bold"}}>Todolist Title</Text>
+                    <Text style={{margin:10,color:"white"}}>Todolist Description</Text>
+                    <Text style={{margin:10,color:"white",top:35}}>Creat Deadline</Text>
+            </View>
+            <View style={{backgroundColor:"#F79E89",height:150,width:"90%",margin:10,borderRadius:20}}>
+                    <Text style={{margin:10,color:"white",fontSize:17,fontWeight:"bold"}}>Todolist Title</Text>
+                    <Text style={{margin:10,color:"white"}}>Todolist Description</Text>
+                    <Text style={{margin:10,color:"white",top:35}}>Creat Deadline</Text>
+            </View>
+            <View style={{backgroundColor:"white",height:60,width:"100%",margin:50,alignItems:"flex-end",top:100}}>
+            <Image
+                        source={require('../assets/images/ekle.png')}
+                        style={{ width: 60, height: 60, margin: 10 }}
+                    />
+            </View>
         </View>
     )
 }
@@ -23,9 +62,9 @@ const HomeScreen = () => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
+        justifyContent: 'flex-start',
         alignItems: 'center',
-        backgroundColor: '#f0f0f0',
+        backgroundColor: '#FFFFFF',
     },
     title: {
         fontSize: 24,
